d32c28c8a3353022184db1fa3d2cd54eaad92d9b
cbitc <2413809064@qq.com>
2023-12-11 18:11:01 +0800

    the registry has all done

--- include/DataStruct.hpp
+++ include/DataStruct.hpp
@@ -6,151 +6,279 @@
 
 namespace CDataStruct
 {
-    template<typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
-    class SparseSet final
-    {
-    public:
-        using value_t = T;
+template<typename T,typename = std::enable_if_t<std::is_integral_v<T>>>
+class SparseSet final
+{
+public:
+    
+    using value_t = T;
 
-        SparseSet() {
-            sparse_.resize(1, null);
+
+    
+    SparseSet() noexcept {
+        sparse_.resize(1,null);
+    }
+
+
+
+    
+
+    void insert(value_t value) noexcept {
+        if (value >= sparse_.size()) {
+            sparse_.resize(value << 1,null);
         }
+        sparse_[value] = pakced_.size();
+        pakced_.push_back(value);
+    }
 
-        void insert(value_t value) {
-            if (value >= sparse_.size()) {
-                sparse_.resize(value << 1, null);
-            }
-            sparse_[value] = pakced_.size();
-            pakced_.push_back(value);
+
+
+
+    
+    bool contain(value_t value) const noexcept {
+        return value < sparse_.size() && sparse_[value] != null;
+    }
+
+
+
+
+    
+    value_t& operator[](value_t value) noexcept {
+        return pakced_[sparse_[value]];
+    }
+
+
+
+
+    
+    const value_t& operator[](value_t value) const noexcept {
+        return pakced_[sparse_[value]];
+    }
+
+
+
+
+    
+    bool remove(value_t value) noexcept {
+        if (!contain(value))return false;
+        value_t index = sparse_[value];
+        pakced_[index] = pakced_.back();
+        pakced_.pop_back();
+        sparse_[pakced_[index]] = index;
+        sparse_[value] = null;
+        return true;
+    }
+
+
+
+
+    
+    size_t size() const noexcept {
+        return pakced_.size();
+    }
+
+
+
+
+    
+    bool empty() const noexcept {
+        return pakced_.empty();
+    }
+
+
+
+
+    
+    void clear() noexcept {
+        sparse_.clear(),pakced_.clear();
+    }
+
+
+
+
+    
+    typename std::vector<value_t>::iterator begin() noexcept {
+        
+        return pakced_.begin();
+    }
+
+
+
+
+    
+    typename std::vector<value_t>::const_iterator begin() const noexcept {
+        return pakced_.begin();
+    }
+
+
+
+
+    
+    typename std::vector<value_t>::iterator end() noexcept {
+        return pakced_.end();
+    }
+
+
+
+
+    
+    typename std::vector<value_t>::const_iterator end() const noexcept {
+        return pakced_.end();
+    }
+
+
+
+
+
+
+
+private:
+    std::vector<value_t> sparse_;
+    std::vector<value_t> pakced_;
+    inline static constexpr value_t null = std::numeric_limits<value_t>::max();
+};
+
+
+
+
+
+
+
+template<typename Key_T,typename Value_T,typename = std::enable_if_t<std::is_integral_v<Key_T>>>
+class DenseMap final
+{
+
+public:
+    
+    using pack_node = std::pair<Key_T,Value_T>;
+
+
+
+
+    
+    DenseMap() noexcept {
+        sparse_.resize(1,null);
+    }
+
+
+
+
+    
+    void insert(Key_T key,const Value_T& value) noexcept {
+        if (key >= sparse_.size()) {
+            sparse_.resize(key << 1,null);
         }
-
-        bool contain(value_t value) const {
-            return value < sparse_.size() && sparse_[value] != null;
-        }
-
-        bool erase(value_t value) {
-            if (!contain(value))return false;
-            value_t index = sparse_[value];
-            pakced_[index] = pakced_.back();
-            pakced_.pop_back();
-            sparse_[pakced_[index]] = index;
-            sparse_[value] = null;
-            return true;
-        }
-
-        size_t size()const {
-            return pakced_.size();
-        }
-
-        bool empty()const {
-            return pakced_.empty();
-        }
-
-        void clear() {
-            sparse_.clear(), pakced_.clear();
-        }
-
-        auto begin() {
-            return pakced_.begin();
-        }
-
-        auto begin()const {
-            return pakced_.begin();
-        }
-
-        auto end() {
-            return pakced_.end();
-        }
-
-        auto end() const {
-            return pakced_.end();
-        }
-
-    private:
-        std::vector<value_t> sparse_;
-        std::vector<value_t> pakced_;
-        inline static constexpr value_t null = std::numeric_limits<value_t>::max();
-    };
+        sparse_[key] = packed_.size();
+        packed_.emplace_back(key,value);
+    }
 
 
 
 
-    template<typename Key_T, typename Value_T, typename = std::enable_if_t<std::is_integral_v<Key_T>>>
-    class DensMap final
-    {
+    
+    void insert(Key_T key) noexcept {
+        insert(key,Value_T{});
+    }
 
-    public:
-        using pack_node = std::pair<Key_T, Value_T>;
 
-        DensMap() {
-            sparse_.resize(1, null);
-        }
 
-        void insert(Key_T key, const Value_T& value) {
-            if (key >= sparse_.size()) {
-                sparse_.resize(key << 1, null);
-            }
-            sparse_[key] = packed_.size();
-            packed_.emplace_back(key, value);
-        }
 
-        void insert(Key_T key) {
-            insert(key, Value_T{});
-        }
+    
+    bool contain(Key_T key) const noexcept {
+        return key < sparse_.size() && sparse_[key] != null;
+    }
 
-        bool contain(Key_T key)const {
-            return key < sparse_.size() && sparse_[key] != null;
-        }
 
-        bool erase(Key_T key) {
-            if (!contain(key))return false;
-            Key_T index = sparse_[key];
-            packed_[index] = std::move(packed_.back());
-            packed_.pop_back();
-            sparse_[packed_[index].first] = index;
-            sparse_[key] = null;
-            return true;
-        }
 
-        size_t size() {
-            return packed_.size();
-        }
 
-        bool empty() {
-            return packed_.empty();
-        }
+    
+    bool remove(Key_T key) {
+        if (!contain(key))return false;
+        Key_T index = sparse_[key];
+        packed_[index] = std::move(packed_.back());
+        packed_.pop_back();
+        sparse_[packed_[index].first] = index;
+        sparse_[key] = null;
+        return true;
+    }
 
-        void clear() {
-            sparse_.clear(), packed_.clear();
-        }
 
-        Value_T& operator[](Key_T key) {
-            return packed_[sparse_[key]].second;
-        }
 
-        const Value_T& operator[](Key_T key)const {
-            return packed_[sparse_[key]].second;
-        }
 
-        auto begin() {
-            return packed_.begin();
-        }
+    
+    size_t size() const noexcept {
+        return packed_.size();
+    }
 
-        auto begin()const {
-            return packed_.begin();
-        }
 
-        auto end() {
-            return packed_.end();
-        }
 
-        auto end()const {
-            return packed_.end();
-        }
 
-    private:
-        std::vector<Key_T> sparse_;
-        std::vector<pack_node> packed_;
-        inline static constexpr Key_T null = std::numeric_limits<Key_T>::max();
-    };
+    
+    bool empty() const noexcept {
+        return packed_.empty();
+    }
+
+
+
+
+    
+    void clear() noexcept {
+        sparse_.clear(),packed_.clear();
+    }
+
+
+
+
+    
+    Value_T& operator[](Key_T key) noexcept {
+        if (!contain(key))insert(key);
+        assertm("out of range! ",contain(key));
+        return packed_[sparse_[key]].second;
+    }
+
+
+
+
+
+    typename std::vector<pack_node>::iterator begin() {
+        return packed_.begin();
+    }
+
+
+
+
+    
+    typename std::vector<pack_node>::const_iterator begin() const {
+        return packed_.begin();
+    }
+
+
+
+
+    
+    typename std::vector<pack_node>::iterator end() {
+        return packed_.end();
+    }
+
+
+
+
+    
+    typename std::vector<pack_node>::const_iterator end() const {
+        return packed_.end();
+    }
+
+
+
+
+
+    
+private:
+    std::vector<Key_T> sparse_;
+    std::vector<pack_node> packed_;
+    inline static constexpr Key_T null = std::numeric_limits<Key_T>::max();
+};
+
+
+
 }
--- include/world.hpp
+++ include/world.hpp
@@ -7,263 +7,484 @@
 #include<tuple>
 
 
+
+
+
 namespace ECS
 {
 
-    class World
+class World
+{
+    friend class Registry;
+
+
+
+
+
+
+    class ComponentContainer final
     {
-        friend class Registry;
-        class ComponentContainer;
-        using ComponentSet = ComponentContainer;
-        using EntityContainer = CDataStruct::DensMap<Entity, ComponentSet>;
+    public:
 
-        class ComponentContainer final
-        {
+        void insert(const ComponentInfo& info) noexcept {
+            componentInfos_.push_back(info);
+        }
 
-        public:
-            void insert(const ComponentInfo& info) {
-                componentInfos_.push_back(info);
+
+
+
+
+        void insert(componentID_t id,void* data) noexcept {
+            componentInfos_.emplace_back(id,data);
+        }
+
+
+
+
+
+        bool contain(componentID_t id) const noexcept {
+            for (const ComponentInfo& info : componentInfos_) {
+                if (info.id == id)return true;
             }
+            return false;
+        }
 
-            void insert(componentID_t id, void* data) {
-                componentInfos_.emplace_back(id,data);
-            }
 
-            void erase(componentID_t id) {
-                std::swap(componentInfos_[id],componentInfos_.back());
-                componentInfos_.pop_back();
-            }
 
-            bool container(componentID_t id)const {
-                for (const ComponentInfo& info : componentInfos_) {
-                    if (info.id == id)return true;
+
+
+        bool remove(componentID_t id) noexcept {
+            for (ComponentInfo& info : componentInfos_) {
+                if (info.id == id) {
+                    std::swap(info,componentInfos_.back()),componentInfos_.pop_back();
+                    return true;
                 }
-                return false;
             }
+            return false;
+        }
 
-            bool remove(componentID_t id) {
-                for (ComponentInfo& info : componentInfos_) {
-                    if (info.id == id) {
-                        std::swap(info, componentInfos_.back()), componentInfos_.pop_back();
-                        return true;
-                    }
-                }
-                return false;
+
+
+
+
+        ComponentInfo& operator[](componentID_t id) noexcept {
+            for (ComponentInfo& info : componentInfos_) {
+                if (info.id == id)return info;
             }
-   
+            assertm("not found!",false);
 
-            ComponentInfo& operator[](componentID_t id) {
-                for (ComponentInfo& info : componentInfos_) {
-                    if (info.id == id)return info;
-                }
-                componentInfos_.emplace_back(id, nullptr);
-                return componentInfos_.back();
+            return componentInfos_.back();
+        }
+
+
+
+
+
+        const ComponentInfo& operator[](componentID_t id) const noexcept {
+            for (const ComponentInfo& info : componentInfos_) {
+                if (info.id == id)return info;
             }
+            assertm("not found!",false);
 
-            
+            return componentInfos_.back();
+        }
 
-            std::vector<ComponentInfo>::iterator begin() {
-                return componentInfos_.begin();
-            }
 
-            std::vector<ComponentInfo>::const_iterator begin() const {
-                return componentInfos_.begin();
-            }
 
-            std::vector<ComponentInfo>::iterator end() {
-                return componentInfos_.end();
-            }
 
-            std::vector<ComponentInfo>::const_iterator end() const {
-                return componentInfos_.end();
-            }
 
-        private:
-            std::vector<ComponentInfo> componentInfos_;
-        };
+        std::vector<ComponentInfo>::iterator begin() noexcept {
+            return componentInfos_.begin();
+        }
 
-        class ComponentsMap final
-        {
-        public:
-            using EntitySet = CDataStruct::SparseSet<Entity>;
-        public:
-            
-            EntitySet& operator[](componentID_t id) {
-                if (id >= map_.size()) {
-                    map_.resize(id + 1);
-                }
-                return map_[id];
-            }
-            
-            std::vector<EntitySet>::iterator begin() {
-                return map_.begin();
-            }
 
-            std::vector<EntitySet>::const_iterator begin()const {
-                return map_.begin();
-            }
 
-            
-            std::vector<EntitySet>::iterator end() {
-                return map_.end();
-            }
 
-            
-            std::vector<EntitySet>::const_iterator end()const {
-                return map_.end();
-            }
-        private:
-            std::vector<EntitySet> map_;
-        };
+
+        std::vector<ComponentInfo>::const_iterator begin() const noexcept {
+            return componentInfos_.begin();
+        }
+
+
+
+
+
+        std::vector<ComponentInfo>::iterator end() noexcept {
+            return componentInfos_.end();
+        }
+
+
+
+
+
+        std::vector<ComponentInfo>::const_iterator end() const noexcept {
+            return componentInfos_.end();
+        }
+
+
+
 
 
     private:
-        EntityContainer entitys_;
-        ComponentsMap componentsMap_;
-    public:
-        void entitysLog()const {
-            for (const auto& [entity,coms] : entitys_) {
-                printf("%d ", entity);
-                printf("(");
-                for (const auto& info : coms) {
-                    printf("%d ", info.id);
-                }
-                printf(")\n");
-            }
-        }
-
-        void componentsMapLog()const {
-            int i = 0;
-            for (const auto& set : componentsMap_) {
-                printf("%d ", i++);
-                printf("(");
-                for (auto entity : set) {
-                    printf("%d ", entity);
-                }
-                printf(")\n");
-            }
-        }
+        std::vector<ComponentInfo> componentInfos_;
     };
 
-    class Registry final
-    {
-        Entity _get_entity() {
-            return EntityManager::spawn();
-        }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    class ComponentMap final
+    {
 
     public:
-        Registry(World& world) :world_(world) {}
 
-        template<typename...Args>
-        auto view() {
-
+        CDataStruct::SparseSet<Entity>& operator[](componentID_t id) noexcept {
+            if (id >= map_.size()) {
+                map_.resize(id + 1);
+            }
+            assertm("the component is not in map",contain(id));
+            return map_[id];
         }
 
-        template<typename T,typename...Args>
-        void emplace(Entity entity, Args&&...args) {
-            auto id = ComponentManager::componentID<T>();
-            auto& components = world_.entitys_[entity];
+
+
+
+
+        bool contain(componentID_t id) const noexcept {
+            return id < map_.size();
+        }
+
+
+
+
+
+        std::vector<CDataStruct::SparseSet<Entity>>::iterator begin() noexcept {
+            return map_.begin();
+        }
+
+
+
+
+
+        std::vector<CDataStruct::SparseSet<Entity>>::const_iterator begin() const noexcept {
+            return map_.begin();
+        }
+
+
+
+
+
+
+        std::vector<CDataStruct::SparseSet<Entity>>::iterator end() noexcept {
+            return map_.end();
+        }
+
+
+
+
+
+
+        std::vector<CDataStruct::SparseSet<Entity>>::const_iterator end() const noexcept {
+            return map_.end();
+        }
+
+
+
+
+
+    private:
+        std::vector<CDataStruct::SparseSet<Entity>> map_;
+    };
+
+
+
+
+
+
+
+
+
+
+
+public:
+    using componentSet_t = ComponentContainer;
+    using entitySet_t = CDataStruct::SparseSet<Entity>;
+    using entityMap_t = CDataStruct::DenseMap<Entity,componentSet_t>;
+    using componentMap_t = ComponentMap;
+
+private:
+    entityMap_t entityMap_;
+    componentMap_t componentMap_;
+
+public:
+    void entitysLog()const {
+        for (const auto& [entity,coms] : entityMap_) {
+            printf("%d ",entity);
+            printf("(");
+            for (const auto& info : coms) {
+                printf("%d ",info.id);
+            }
+            printf(")\n");
+        }
+    }
+
+    void componentsMapLog()const {
+        int i = 0;
+        for (const auto& set : componentMap_) {
+            printf("%d ",i++);
+            printf("(");
+            for (auto entity : set) {
+                printf("%d ",entity);
+            }
+            printf(")\n");
+        }
+    }
+};
+
+
+
+
+
+
+
+
+class Registry final
+{
+
+    Entity _get_entity() const {
+        return EntityManager::spawn();
+    }
+
+
+
+
+
+
+public:
+
+    Registry(World& world) :world_(world) {}
+
+
+
+
+
+    template<typename T,typename...Args>
+    std::vector<Entity> view() noexcept {
+        componentID_t id = ComponentManager::componentID<T>();
+        const World::entitySet_t& entitySet = world_.componentMap_[id];
+        std::vector<Entity> entitys;
+        for (Entity entity : entitySet) { entitys.push_back(entity); }
+        if constexpr (sizeof...(Args) != 0) {
+            doView<Args...>(entitys);
+        }
+        return entitys;
+    }
+
+
+
+
+
+    template<typename T,typename...Args>
+    auto emplace(Entity entity,Args&&...args) noexcept ->
+        std::void_t<decltype(new T{ std::forward<Args>(args)... }) > {
+        componentID_t id = ComponentManager::componentID<T>();
+        World::componentSet_t& components = world_.entityMap_[entity];
+        components.insert(id,ComponentManager::create<T>(std::forward<Args>(args)...));
+        world_.componentMap_[id].insert(entity);
+    }
+
+
+
+
+
+    template<typename T,typename...Args>
+    auto replace(Entity entity,Args&&...args) noexcept ->
+        std::void_t<decltype(new T{ std::forward<Args>(args)... }) > {
+        componentID_t id = ComponentManager::componentID<T>();
+        World::componentSet_t& components = world_.entityMap_[entity];
+        *(static_cast<T*>(components[id].data)) = { std::forward<Args>(args)... };
+    }
+
+
+
+
+
+    template<typename T,typename...Args>
+    auto emplace_or_replace(Entity entity,Args&&...args) noexcept ->
+    std::void_t<decltype(new T{ std::forward<Args>(args)... }) > {
+        componentID_t id = ComponentManager::componentID<T>();
+        World::componentSet_t& components = world_.entityMap_[entity];
+        if (!components.contain(id)) {
             components.insert(id,ComponentManager::create<T>(std::forward<Args>(args)...));
-            world_.componentsMap_[id].insert(entity);
+            world_.componentMap_[id].insert(entity);
+        } else {
+            *(static_cast<T*>(components[id].data)) = { std::forward<Args>(args)... };
         }
+    }
 
-        template<typename...Args>
-        void replace(Entity entity, Args&&...args) {
 
+
+
+
+    template<typename T,typename...Args>
+    bool all_of(Entity entity) const noexcept {
+        componentID_t id = ComponentManager::componentID<T>();
+        bool is = world_.entityMap_[entity].contain(id);
+        if constexpr (sizeof...(Args) == 0) {
+            return is;
+        } else {
+            return is && all_of<Args...>(entity);
         }
+    }
 
-        template<typename...Args>
-        void emplace_or_replace(Entity entity, Args&&...args) {
 
+
+
+
+    template<typename T,typename...Args>
+    bool any_of(Entity entity) const noexcept {
+        componentID_t id = ComponentManager::componentID<T>();
+        bool is = world_.entityMap_[entity].contain(id);
+        if constexpr (sizeof...(Args) == 0) {
+            return is;
+        } else {
+            return is || all_of<Args...>(entity);
         }
+    }
 
-        template<typename T,typename...Args>
-        bool all_of(Entity entity) {
-            componentID_t id = ComponentManager::componentID<T>();
-            bool is = world_.entitys_[entity].container(id);
-            if constexpr (sizeof...(Args) == 0) {
-                return is;
-            } else {
-                return is && all_of<Args...>(entity);
+
+
+
+
+    template<typename T,typename...Args>
+    void remove(Entity entity) noexcept {
+        componentID_t id = ComponentManager::componentID<T>();
+        world_.entityMap_[entity].remove(id);
+        world_.componentMap_[id].remove(entity);
+        if constexpr (sizeof...(Args) != 0) {
+            remove<Args...>(entity);
+        }
+        return;
+    }
+
+
+
+
+
+    template<typename T,typename...Ts>
+    auto get(Entity entity) noexcept {
+        componentID_t id = ComponentManager::componentID<T>();
+        T* data = static_cast<T*>(world_.entityMap_[entity][id].data);
+        if constexpr (sizeof...(Ts) == 0) {
+            return data;
+        } else {
+            return std::make_tuple(data,get<Ts>(entity)...);
+        }
+    }
+
+
+
+
+
+    template<typename T,typename...Ts>
+    auto try_get(Entity entity) noexcept {
+        componentID_t id = ComponentManager::componentID<T>();
+        const World::componentSet_t& components = world_.entityMap_[entity];
+        T* data = components.contain(id) ? static_cast<T*>(components[id].data) : nullptr;
+        if constexpr (sizeof...(Ts) == 0) {
+            return data;
+        } else {
+            return std::make_tuple(data,try_get<Ts>(entity)...);
+        }
+    }
+
+
+
+
+
+    template<typename T,typename...Ts>
+    std::conditional_t < sizeof...(Ts) == 0,
+    decltype(new T{}),std::tuple<T*,std::add_pointer_t<Ts>... >>
+    get_or_emplace(Entity entity) noexcept {
+        componentID_t id = ComponentManager::componentID<T>();
+        World::componentSet_t& components = world_.entityMap_[entity];
+        T* data = nullptr;
+        if (!components.contain(id)) {
+            data = ComponentManager::create<T>();
+            components.insert(id,data);
+            world_.componentMap_[id].insert(entity);
+        } else {
+            data = static_cast<T*>(components[id].data);
+        }
+        if constexpr (sizeof...(Ts) == 0) {
+            return data;
+        } else {
+            return std::make_tuple(data,get_or_emplace<Ts>(entity)...);
+        }
+    }
+
+
+
+
+
+    Entity create() noexcept {
+        Entity entity = _get_entity();
+        world_.entityMap_.insert(entity);
+        return entity;
+    }
+
+
+
+
+
+    void destory(Entity entity) noexcept {
+        World::componentSet_t& components = world_.entityMap_[entity];
+        for (ComponentInfo& info : components) {
+            world_.componentMap_[info.id].remove(entity);
+            ComponentManager::destory(info);
+        }
+        world_.entityMap_.remove(entity);
+        EntityManager::destory(entity);
+    }
+
+
+
+
+
+
+private:
+
+    template<typename T,typename...Args>
+    void doView(std::vector<Entity>& entitys) {
+        componentID_t id = ComponentManager::componentID<T>();
+        for (size_t i = 0;i < entitys.size();i++) {
+            Entity& entity = entitys[i];
+            if (!world_.entityMap_[entity].contain(id)) {
+                std::swap(entity,entitys.back()),entitys.pop_back();
             }
-
         }
-
-        template<typename T,typename...Args>
-        bool any_of(Entity entity) {
-            componentID_t id = ComponentManager::componentID<T>();
-            bool is = world_.entitys_[entity].container(id);
-            if constexpr (sizeof...(Args) == 0) {
-                return is;
-            } else {
-                return is || all_of<Args...>(entity);
-            }
+        if constexpr (sizeof...(Args) != 0) {
+            doView<Args...>(entitys);
         }
-
-        template<typename T,typename...Args>
-        void erase(Entity entity) {
-            componentID_t id = ComponentManager::componentID<T>();
-            world_.entitys_[entity].erase(id);
-            world_.componentsMap_[id].erase(entity);
-            if constexpr (sizeof...(Args) != 0) {
-                erase<Args...>(entity);
-            }
-            return;
-        }
-
-        template<typename T,typename...Args>
-        void remove(Entity entity) {
-            componentID_t id = ComponentManager::componentID<T>();
-            if (bool is = world_.entitys_[entity].remove(id);is) {
-                world_.componentsMap_[id].erase(entity);
-            }
-            if constexpr (sizeof...(Args) != 0) {
-                remove<Args...>(entity);
-            }
-            return;
-        }
-
-        template<typename T,typename...Args>
-        auto get(Entity entity) {
-            componentID_t id = ComponentManager::componentID<T>();
-            T& data = *(static_cast<T*>(world_.entitys_[entity][id].data));
-            if constexpr (sizeof...(Args) == 0) {
-                return std::tie(data);
-            } else {
-                return std::tuple_cat(
-                    std::tie(data),
-                    get<Args...>(entity)
-                );
-            }
-        }
-
-        template<typename T,typename...Args>
-        auto try_get(Entity entity) {
-            
-        }
+    }
 
 
-        Entity create() {
-            Entity entity = _get_entity();
-            world_.entitys_.insert(entity);
-            return entity;
-        }
-
-        void destory(Entity entity) {
-            auto& components = world_.entitys_[entity];
-            for (ComponentInfo& info : components) {
-                world_.componentsMap_[info.id].erase(entity);
-                ComponentManager::destory(info);
-            }
-            world_.entitys_.erase(entity);
-            EntityManager::destory(entity);
-        }
-    private:
-        
+private:
+    World& world_;
+};
 
 
-    private:
-        World& world_;
-    };
+
+
 }
